287242a78b7bf0965d9d3a2208de95a62e8f866d
Clayton <217299695@mycput.ac.za>
2020-07-05 15:40:26 +0200

    Update tests on IssueFactoryTest.java Comment inserted on IssueFactory.java

--- src/test/java/com/group12/factory/IssueFactoryTest.java
+++ src/test/java/com/group12/factory/IssueFactoryTest.java
@@ -6,11 +6,26 @@ import org.junit.Test;
 
 public class IssueFactoryTest {
 
+    Issue issueDummy1;
+    Issue issueDummy2;
+
     @Test
-    public void testCreateIssue() {
+    public void assertIssueID_IssueRaisedDate_NotNull() {
 
-        Issue issue = IssueFactory.createIssue("Safety","Broken mirrors in the toilet","MEDIUM");
+        issueDummy1 = IssueFactory.createIssue("","","");
 
+        Assert.assertNotNull(issueDummy1.getIssueID());
+        Assert.assertNotNull(issueDummy1.getIssueRaisedDate());
+
+    }
+
+    @Test
+    public void assertIssueID_Unique(){
+
+        issueDummy1 = IssueFactory.createIssue("Health", "Expired cafeteria food", "HIGH");
+        issueDummy2 = IssueFactory.createIssue("Safety","Broken toilet mirrors","HIGH");
+
+        Assert.assertNotEquals(issueDummy1.getIssueID(), issueDummy2.getIssueID());
     }
 
 
